d2f1b48fa580e4133b5afb249179db98f93e4b2d
noahcanen <ncanen@uwyo.edu>
2025-04-07 17:00:20 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,27 +2,30 @@ function depthFirstSearch(graph, startNode, targetNode) {
     visited = [startNode]
     node = [startNode]
     path = []
-    console.log("1")
 
-    path = nodeChecker(graph, startNode, targetNode,visited,node,path)
+
+    path = nodeChecker(graph, startNode, targetNode, visited, node, path)
     if (path.includes(targetNode)){return path}
     return [];
 }
-function nodeChecker(graph, startNode, targetNode,visited,node,path){
+function nodeChecker(graph, startNode, targetNode, visited, node, path){
 
     let check = node.pop()
     visited.push(check)
     path.push(check)
     if (check == targetNode){return path}
-    for(edge  in graph[check])
+
+    for(let edge in graph[check])
     {
+
         let next = graph[check][edge]
         if (!visited.includes(next)){
             node.push(next)
             nodeChecker(graph, startNode, targetNode,visited,node,path)
         }
         if (path.includes(targetNode)){return path}
-        path.pop()
+        if (path.length > 1) path.pop()
     }
+    
     return path
 }
